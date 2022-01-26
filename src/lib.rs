51bd9f5a609b42eda90698ee9b6171c70660e344
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-26 10:52:12 +1030

    init logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
+#[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
+struct Card(u8, char);
+impl Card {}
+
+#[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
 enum Hand {}
-enum Card {}
-enum Sorted {}
+impl Hand {
+    // sorting
+    // find straight & flush
+}
+
+
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     unimplemented!("Out of {:?}, which hand wins?", hands)
