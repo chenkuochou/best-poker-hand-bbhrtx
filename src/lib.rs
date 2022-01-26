59e50412c3059adece39750ea2de843118d260f2
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-27 10:24:08 +1030

    Hand trait logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+// Cards combination eg. 4D
 #[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
-// Found cards eg. 4S
 struct Card(u8, char);
 impl Card {
     fn new(card_str: &str) -> Self {
@@ -17,16 +17,57 @@ impl Card {
     }
 }
 
+// Hands of poker patterns
 #[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
-// Found hands of poker patterns
-enum Hand {}
+enum Hand {
+    HighCard(u8, u8, u8, u8, u8),
+    OnePair(u8, u8, u8, u8),
+    TwoPair(u8, u8, u8),
+    ThreeOfAKind(u8, u8, u8),
+    Straight(u8),
+    Flush(u8, u8, u8, u8, u8),
+    FullHouse(u8, u8),
+    FourOfAKind(u8, u8),
+    StraightFlush(u8),
+}
 impl Hand {
-    // sorting
-    // find straight & flush
+    fn new(cards: &mut [Card]) -> Self {
+        use Hand::*;
 
-    // matching
+        // sorting
+        cards.sort_by(|x, y| y.cmp(x));
+        println!("sorted:{:?}", cards);
+        // [Card(5, 'H'), Card(5, 'D'), Card(4, 'S'), Card(4, 'H'), Card(4, 'D')]
+        
+        // straight & flush vars
+        let straight = cards.windows(); //i[0].0 == i[1].0+1
+        let flush = cards.windows(); // i[0]1 == i[1].1
+
+        // matching straight & flush combinations
+        // straight + flush = StraightFlush
+        // !(straight || flush) + 4 same cards = FourOfAKind
+        // !(straight || flush) + 2 same card pair = TwoPair
+
+        // need a parttern for poker patterns, counted and sorted
+        // (count, number)
+        // eg. full house: [(3, 5), (2, 9)] from ["5H 5S 5D 9S 9D", "5H 5S 5D 8S 8D"]
+
+    }
 }
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
-    unimplemented!("Out of {:?}, which hand wins?", hands)
+    // hands.iter() 
 }
+
+
+
+/* Resources */
+// lifetimes explainer
+// https://doc.rust-lang.org/reference/lifetime-elision.html
+// https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
+//
+// map
+// https://doc.rust-lang.org/rust-by-example/error/option_unwrap/map.html?highlight=.map#combinators-map
+//
+// sort_unstable_by 
+// https://doc.rust-lang.org/std/primitive.slice.html#method.sort_unstable_by
