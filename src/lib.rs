840737b545856217bc5d44c1d1a571aaf41e56cb
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-26 11:54:57 +1030

    Card built

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,31 @@
 #[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
+// Found cards eg. 4S
 struct Card(u8, char);
-impl Card {}
+impl Card {
+    fn new(card_str: &str) -> Self {
+        let bytes = card_str.as_bytes();
+        let rank: u8 = match &bytes[0] {
+            b'A' => 14,
+            b'K' => 13,
+            b'Q' => 12,
+            b'J' => 11,
+            b'1' => 10,
+            c => c - b'0',
+        };
+        let suit = bytes[bytes.len() - 1];
+        Card(rank, suit as char)
+    }
+}
 
 #[derive(Debug, Ord, Eq, PartialOrd, PartialEq)]
+// Found hands of poker patterns
 enum Hand {}
 impl Hand {
     // sorting
     // find straight & flush
+
+    // matching
 }
-
-
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     unimplemented!("Out of {:?}, which hand wins?", hands)
