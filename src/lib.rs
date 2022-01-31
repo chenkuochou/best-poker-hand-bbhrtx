fa8464329ef2ddcca4384365097cffdb1a32c517
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-01-31 22:11:43 +1030

    update winning_hands

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ impl Hand {
         /* Sorting */
         cards.sort_by(|x, y| y.cmp(x));
         // println!("sorted:{:?}", cards);
-        // [Card(5, 'H'), Card(5, 'D'), Card(4, 'S'), Card(4, 'H'), Card(4, 'D')]
+        // [Card(8, 'D'), Card(5, 'H'), Card(4, 'S'), Card(4, 'H'), Card(4, 'C')]
 
 
         /* Sorted parttern (number, count) for each card */
@@ -47,20 +47,19 @@ impl Hand {
             .map(|n| (n, cards.iter().filter(|i| i.0 == n).count() as u8))
             .filter(|(_, count)| *count != 0)
             .collect::<Vec<_>>();
-        // println!("number_count:{:?}", number_count);
 
 
-        number_count.sort_by(|x, y| y.1.cmp(&x.1).then(y.0.cmp(&x.0)));
+        number_count.sort_unstable_by(|x, y| (y.1, y.0).cmp(&(x.1, x.0)));
         // I asked for the above structure 
         // https://stackoverflow.com/questions/70919627/how-to-sort-items-inside-a-vector-in-rust
 
 
         let nc = number_count.as_slice();
         // println!("nc:{:?}", nc);
-        // [(4, 3), (8, 1), (5, 1)] from {"4S 5H 4C 8D 4H"}
+        // [(4, 3), (8, 1), (5, 1)]
 
 
-        /* Match patterns & return rank(s) */
+        /* Match patterns by rank */
         let straight = cards.windows(2).all(|w| w[0].0 == w[1].0 + 1);
         let flush = cards.windows(2).all(|w| w[0].1 == w[1].1);
 
@@ -82,33 +81,61 @@ impl Hand {
 }
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
-    // unimplemented!("Out of {:?}, which hand wins?", hands)
-
     // The returned referecne 'Option<>' will also be valid for the length of 'hands'
     // CANNOT return a referecne to somethig created inside the fucntion!
 
-    // hands.iter()
-    // compare pattern then rank
-    
+    // println!("original hands:{:?}", hands);
+    // ["4S 5H 4C 8D 4H", "4D AH 3S 2D 5C"]
 
+
+    /* Feed Hand with Card */
+    let mut hands = hands
+        .iter()
+        .map(|h| {
+            let hand = Hand::new(
+                h.split(|c: char| c.is_whitespace())
+                 .map(|c| Card::new(c))
+                 .collect::<Vec<_>>()
+                 .as_mut_slice(),
+            );
+            (hand, *h)
+        })
+        .collect::<Vec<_>>();
+    hands.sort_by(|x, y| y.cmp(x));
+    // println!("hands:{:?}", hands);
+    // [(ThreeOfAKind(4, 8, 5), "4S 5H 4C 8D 4H"), (HighCard(14, 5, 4, 3, 2), "4D AH 3S 2D 5C")]
+
+
+    /* Pull the winner */
+    let winner = hands
+        .iter()
+        .filter(|h| hands[0].eq(&h))
+        .map(|h| h.1)
+        .collect::<Vec<&'a str>>();
+    Some(winner)
 }
+
+
+/* To-do */
+// fixing 'test_aces_can_end_a_straight_low' FAILED
+// fixing 'test_a_tie_has_multiple_winners' FAILED
+// redesign Card
+// refactor matching patterns in 'impl Hand'
 
 /* Resources */
 // lifetimes explainer
 // https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
 // a borrow checker will verify the length of each scope in compile time
-// the smaller lifetime will be still valid"
+// the smaller lifetime will be still valid
 //
 // map()
-// https://doc.rust-lang.org/rust-by-example/error/option_unwrap/map.html?highlight=.map#combinators-map
-// sort_unstable_by()
-// https://doc.rust-lang.org/std/primitive.slice.html#method.sort_unstable_by
+// https://doc.rust-lang.org/rust-by-example/error/option_unwrap/map.html
 // slice.windows()
 // https://doc.rust-lang.org/std/primitive.slice.html#method.windows
+// sort_unstable_by()
+// https://doc.rust-lang.org/std/primitive.slice.html#method.sort_unstable_by
 // as_slice()
 // https://doc.rust-lang.org/std/vec/struct.Vec.html#method.as_slice
-// as_mut_slice()
-// https://doc.rust-lang.org/std/vec/struct.Vec.html#method.as_mut_slice
 //
 // Rust poker hand ranker (complex..)
 // https://github.com/pieterdopheide/poker
